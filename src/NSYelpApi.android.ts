de0e849afb9e1857715eb6db8fb228890fc14f31
Jonathan Rinciari <jrinciaritosd@gmail.com>
2019-03-11 10:55:32 -0400

    Added android support

--- src/NSYelpApi.android.ts
+++ src/NSYelpApi.android.ts
@@ -1,5 +1,25 @@
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
 import { Common } from './NSYelpApi.common';
 
 export class NSYelpApi extends Common {
+  _client: com.yelp.fusion.client.connection.YelpFusionApi;
+  constructor(apiKey: string) {
+    super();
+    this._client = new com.yelp.fusion.client.connection.YelpFusionApiFactory().createAPI(apiKey);
+    console.dir(this._client);
+    // console.dir(com.yelp.fusion.client.BuildConfig.APPLICATION_ID);
+    // console.dir(com.yelp.fusion.client.connection);
+    // console.dir(com.yelp.fusion.client);
+    // console.dir(new com.yelp.fusion.client());
+    // let test = new com.yelp.fusion.client.connection.YelpFusionApiFactory();
+    // console.dir(test);
+    // this._client = test.createAPI(apiKey);
+  }
 
+  businessSearchWithId() {
+    return new Promise((r, p) => {
+      r();
+    });
+    // console.log(this._client.getPhoneSearch(phone));
+  }
 }
--- src/references.d.ts
+++ src/references.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+// / <reference path="./typings/java!YelpApi.ts" />
+/// <reference path="./typings/NSYelpApi.d.ts" />
+/// <reference path="./typings/objc!YelpAPI.d.ts" />
